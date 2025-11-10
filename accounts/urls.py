1031ec1f785222c0cf1cbefc58cebe29547ee231
Bhavani <bhavani@testpress.in>
2025-11-10 11:37:22 +0000

    Fix password change templates:clean up form

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,32 +3,42 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
+
+    # Password change
     path('password/change/', 
         auth_views.PasswordChangeView.as_view(
             template_name="registration/password_change_form.html"
         ),
-        name='password_change'),
-
+        name='password_change'
+    ),
     path('password/change/done/', 
         auth_views.PasswordChangeDoneView.as_view(
             template_name="registration/password_change_done.html"
         ),
-        name='password_change_done'),
-
-
-    path(
-        'password/reset/',
-        auth_views.PasswordResetView.as_view(
-            email_template_name="registration/password_reset_email.html",
-        ),
-        name="password_reset"
+        name='password_change_done'
     ),
-    path('password/reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),        
-    path('home/', views.home, name='home'),
 
+    # Password reset
+    path('password/reset/', 
+        views.CustomPasswordResetView.as_view(
+            template_name="registration/password_reset_form.html",       # form user sees
+            email_template_name="registration/password_reset_email.html", # HTML email
+            subject_template_name="registration/password_reset_subject.txt"
+        ),
+        name='password_reset'
+    ),
+    path('password/reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='registration/password_reset_done.html'
+    ), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='registration/password_reset_confirm.html'
+    ), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='registration/password_reset_complete.html'
+    ), name='password_reset_complete'),
+
+    path('home/', views.home, name='home'),
 ]
