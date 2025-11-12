f33b6513009b21653dab49a9512abaa8c9b599a4
Bhavani <152827999+bhavani-b08@users.noreply.github.com>
2025-11-12 15:54:19 +0530

    Add User Authentication in Restaurant app
    * Add full user authentication support:
    - Implement signup, login, and logout
    - Add password reset via email
    - Add password change for authenticated users
    - Configure and customize authentication templates
    
    * customize the password reset and change templates
    
    * Fix password change templates:clean up form
    
    * refactor:replace the signup view with generic view(createview)
    
    * refactor:replace custom passwordresetview with built in view
    
    * feature:user authentication system (login, logout, signup)
    
    * refactor:remove the signup form
    
    * refactor:remove extra spaces in settings.py
    
    * fix:make the settings.py as same as in the main branch
    
    * fix:make the settings.py as same as in the main branch
    
    * fix:Changes made according to conflicts in PR request
    
    * fix:resolve conflicts in PR
    
    * refactor:use tailwind css for styles
    
    * add newline in accounts/views.py
    
    * feat:Add background image for login and signup page
    
    * refactor:Improve UI of login and signup page
    
    ---------
    
    Co-authored-by: Bhavani <bhavani@testpress.in>

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.contrib.auth import login
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+
+class SignUpView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
