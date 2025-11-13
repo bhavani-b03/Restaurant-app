00dc2bca9abe33c227bd402632feb8355aa08087
Bhavani <152827999+bhavani-b08@users.noreply.github.com>
2025-11-13 16:26:46 +0530

    feat: Add Password Reset Feature (#7)
    Added a Forgot Password link on the login page.
    Implemented the password reset flow via email, including:
    1. Password reset request page (enter email).
    2. Email sent confirmation page.
    3. Set new password page (accessed via link in email).
    4. Password reset complete confirmation page.

--- homebite/settings.py
+++ homebite/settings.py
@@ -122,3 +122,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from decouple import config
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
