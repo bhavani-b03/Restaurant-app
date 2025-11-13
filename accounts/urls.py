c36c15340aba01a44357ccce8b4ad6ac90786508
Bhavani <bhavani@testpress.in>
2025-11-13 07:40:26 +0000

    feat:add password change feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -44,4 +44,16 @@ urlpatterns = [
              template_name='accounts/password_reset_complete.html'
          ), 
          name='password_reset_complete'),
+
+    path('password_change/', 
+         auth_views.PasswordChangeView.as_view(
+             template_name='accounts/password_change.html'
+         ), 
+         name='password_change'),
+
+    path('password_change/done/', 
+         auth_views.PasswordChangeDoneView.as_view(
+             template_name='accounts/password_change_done.html'
+         ), 
+         name='password_change_done'),
 ]
