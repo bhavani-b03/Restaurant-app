b9ec6d2dde7eb9b55b65bc06fa2237cf4ae447b7
Bhavani <152827999+bhavani-b08@users.noreply.github.com>
2025-11-14 11:54:59 +0530

    feat:connect this project to the remote db
    - Database is hosted on the website supabase.
    - Configure Django project to connect to the remote database which is PostgreSQL

--- homebite/settings.py
+++ homebite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,13 +74,15 @@ WSGI_APPLICATION = 'homebite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
+from dotenv import load_dotenv
 
+load_dotenv()
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+DATABASES = {
+    'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
