2a43d9ad5603af65c0ea14f21264bb5e604521b0
Bhavani <bhavani@testpress.in>
2025-11-14 06:36:16 +0000

    feat: register models in admin site

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import * 
+
+admin.site.register(Cuisine)
+admin.site.register(Restaurant)
+admin.site.register(Food)
+admin.site.register(RestaurantImage)
+admin.site.register(Review)
+admin.site.register(Bookmark)
+admin.site.register(Visited)
