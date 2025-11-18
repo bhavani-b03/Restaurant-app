9ea9f52d9656296797b2ba4c632a530c19c427d2
Bhavani <152827999+bhavani-b08@users.noreply.github.com>
2025-11-18 18:12:56 +0530

    feat : Add restaurant list page
    Implemented RestaurantListView using Django’s generic ListView.
    Added restaurant_list.html template with structured layout for:
    - restaurant name
    - rating
    - cuisines
    - diet type badge
    - average cost

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Restaurant
+from django.views.generic import ListView
 
 # Create your views here.
+
+class RestaurantListView(ListView):
+    model = Restaurant
+    template_name = "restaurants/list.html"  
+    context_object_name = "restaurants"  
+    paginate_by = 10  
+
+    ordering = ['-average_rating']
+
+    def get_queryset(self):
+        qs = super().get_queryset().prefetch_related('images')  # fetch images to avoid extra queries
+        return qs
