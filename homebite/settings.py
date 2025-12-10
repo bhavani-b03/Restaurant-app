27174cea365747191d4ed35b4d1a15411e31ef93
Balamurugan-testpress <balamurugan@testpress.in>
2025-12-10 12:32:00 +0530

    feat:configure database settings to use python anywhere database

--- homebite/settings.py
+++ homebite/settings.py
@@ -76,14 +76,15 @@ WSGI_APPLICATION = 'homebite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-import dj_database_url
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DATABASE_URL = os.environ.get('DATABASE_URL')
 DATABASES = {
-    'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bhavanib08$homebite-db',
+        'USER': 'bhavanib08',
+        'PASSWORD': 'Homebite123',
+        'HOST': 'bhavanib08.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+    }
 }
 
 # Password validation
